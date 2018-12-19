4ec64896f3635dda2f617d1f9acb4620e0832c55
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2018-12-19 16:46:40 -0500

    contact us / guarantee / materialize added

---
+++ js/init.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    // var contact_us = document.querySelectorAll('.contact_us');
+    var options = {
+        minDate: new Date()
+    }
+    var instances = M.Datepicker.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var options = {
+        
+    }
+    var instances = M.FormSelect.init(elems, options);
+  });
