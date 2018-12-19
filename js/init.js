e3b7274232b037a56557c95d2dd1de66b5b8d810
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2018-12-19 18:45:47 -0500

    added google analytics

--- js/init.js
+++ js/init.js
@@ -14,3 +14,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     var instances = M.FormSelect.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var options = {};
+    var instances = M.Collapsible.init(elems, options);
+  });
