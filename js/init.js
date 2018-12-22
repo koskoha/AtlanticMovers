eb9766490820aafda0d93741e8da7efe95648852
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2018-12-21 23:58:22 -0500

    added mobile nav

--- js/init.js
+++ js/init.js
@@ -22,3 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     var instances = M.Collapsible.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {}
+    var instances = M.Sidenav.init(elems, options);
+  });
